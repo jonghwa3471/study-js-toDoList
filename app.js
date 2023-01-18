cd3988a72b7e2b2adc7ea1e5f05cf846ea1b1440
jonghwa3471 <113017764+jonghwa3471@users.noreply.github.com>
2023-01-18 16:37:43 +0900

    add_4.5
    add_4.5

---
+++ app.js
@@ -0,0 +1,16 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  loginForm.classList.add(HIDDEN_CLASSNAME);
+  greeting.innerText = `Hello ${username}`;
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+  localStorage.setItem("username", username);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
