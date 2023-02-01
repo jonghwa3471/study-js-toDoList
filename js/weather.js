4138fe50d4457537383f085236892770e765c611
jonghwa <whdghk3471@gmail.com>
2023-02-01 23:08:39 +0900

    all_finish
    all_finish

--- js/weather.js
+++ js/weather.js
@@ -2,8 +2,15 @@ const API_KEY = "58fbb0afdff16aa34f8033c834ac6ee8";
 function onGeoOk(position) {
   const lat = position.coords.latitude;
   const lon = position.coords.longitude;
-  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-  fetch(url);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const weather = document.querySelector("#weather span:first-child");
+      const city = document.querySelector("#weather span:last-child");
+      city.innerText = data.name;
+      weather.innerText = `${data.weather[0].main} / ${data.main.temp} `;
+    });
 }
 
 function onGeoError() {
