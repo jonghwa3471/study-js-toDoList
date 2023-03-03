6ba9d41807346551a90eb9195ed068e41626dd97
jonghwa <whdghk3471@gmail.com>
2023-03-03 23:29:15 +0900

    add_to-do-box
    add_to-do-box

--- js/greetings.js
+++ js/greetings.js
@@ -1,6 +1,9 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
+const statusName = document.querySelector(
+  ".status-bar:first-child span:nth-child(2)"
+);
 
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
@@ -15,6 +18,7 @@ function onLoginSubmit(event) {
 
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
+  statusName.innerText = `${username}'s todo-list`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
